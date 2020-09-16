c257003fe08325aa8e781757e4d5683536bf2bfd
Damien Gaignon <damien.gaignon@gmail.com>
2020-09-16 21:47:20 +0200

    Update service worker

--- js/sw.js
+++ js/sw.js
@@ -23,3 +23,30 @@ self.addEventListener("install", (event) => {
     );
     console.log("Service Worker: Installed");
 });
+
+self.addEventListener('activate', event => {
+    const currentCaches = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
